b3f68c82de96e2301c09b868e6406cbe7fc5d26f
Dev_wanjohi <92452113+wanjohimiriam@users.noreply.github.com>
2023-06-06 12:31:43 +0300

    JAVA Toast

--- app/src/main/java/com/example/firstjavaapp/MainActivity.java
+++ app/src/main/java/com/example/firstjavaapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.firstjavaapp;
 
 import  androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Context context = getApplicationContext();
+        CharSequence text = "Hello, Toast!";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
+    public void disable( View v){
+        v.setEnabled(false);
+        //Log.d("success", "it is working");
+    }
+
 }
